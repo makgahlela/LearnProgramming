aa222951d315e34119f11529211e738695a464b1
kgets <kgets@KGETSA>
2022-03-18 05:28:52 +0200

    Add project files.

---
+++ LearnProgramming/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace LearnProgramming
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            Console.Read();
+        }
+
+    }//class
+}
+
+
---
+++ LearnProgramming/Surface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LearnProgramming
+{
+    internal class Surface
+    {
+
+        private int left;
+        private int top;
+
+        public void BeginDraw()
+        {
+            Console.Clear();
+            left = Console.CursorLeft;
+            top = Console.CursorTop;
+        }
+
+        public void DrawAt(char c, Position position)
+        {
+            try
+            {
+                Console.SetCursorPosition(left + position.X, top + position.Y);
+                Console.Write(c);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
